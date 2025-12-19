94f208af848e02881c810b5df1f8715c21b6a32b
Ahmad <Ahmadkaleeem1@gmail.com>
2025-12-19 05:15:16 +0500

    Ensured valid messages sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 using namespace std;
+class CommandHandler{
+private: 
+unordered_map<string,bool> valid_commands;
+public:
+ CommandHandler() {
+  valid_commands_intializer();
+ }
+ void valid_commands_intializer(){
+  valid_commands = {
+    
+  };
+ }
+ bool is_valid(const string command){
+  if (valid_commands.count(command) > 0) return true; // count return 1 if key exist 
+  else return false;
+ }
+
+ void execute(const string command){
+  if (!is_valid(command) ){
+    cout << "[Error] Unknown Command: '"<< command <<"' \n";
+  }
+  else {
+     cout << "[Success] Executing Command: '"<< command <<"' \n";
+  }
+
+ }
+};
 int main() {
   
   cout << unitbuf;
