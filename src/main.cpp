323156cd0ef5c9fa909427eb5c29db7ac1ea7f6f
Ahmad <Ahmadkaleeem1@gmail.com>
2025-12-19 07:36:12 +0500

    Implemeting the type command with path variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
+#include <vector>
+#include <cstdlib> // To use getenv()
 #include <sstream>
 #include <unordered_map>
 using namespace std;
+namespace fs = filesystem;
 class CommandHandler
 {
 private:
@@ -20,7 +24,7 @@ public:
         {"exit", true},
         {"type", true}};
   }
-  bool is_valid(const string &command)
+  bool is_valid_command(const string &command)
   {
     if (valid_commands.count(command) > 0)
       return true; // count return 1 if key exist
@@ -28,7 +32,7 @@ public:
       return false;
   }
   // function to execute a command
-  void execute(const string &command)
+  bool execute(const string &command)
   {
     stringstream ss(command); // spliting command name and input
     string command_name, command_input;
@@ -41,9 +45,9 @@ public:
     }
     if (command_name == "exit")
     {
-      return;
+      return false ;
     } // return on exit command
-    if (!is_valid(command_name))
+    if (!is_valid_command(command_name))
     {
       cout << command_name << ": command not found\n";
     }
@@ -51,9 +55,11 @@ public:
     {
       execute_echo(command_input);
     }
-    else if (command_name == "type"){
+    else if (command_name == "type")
+    {
       execute_type(command_input);
     }
+    return true;
   }
 
   // function to execute echo command
@@ -63,26 +69,68 @@ public:
   }
   void execute_type(const string &command)
   {
-    if (is_valid(command))
+    if (is_valid_command(command))
     {
       cout << command << " is a shell builtin\n";
     }
-    else {
-         cout << command << ": not found\n";
+    else
+    {
+      string full_path = search_in_path(command);
+      if (full_path.empty())
+      {
+        cout << command << ": not found\n";
+        return;
+      }
+      else
+      {
+        cout << command << " is " << full_path << endl;
+      }
     }
-    
+  }
+
+  string search_in_path(string command)
+  {
+    const char *path_env = getenv("PATH"); // path env return char*
+    if (!path_env)
+      return "";
+    string path_str(path_env); // convertin to string
+    stringstream ss(path_str);
+    string directory;
+    while (getline(ss, directory, ':'))
+    {
+      string full_path = directory + "/" + command;
+      if (check_file_execute(full_path))
+      {
+        return full_path;
+      }
+    }
+    return "";
   }
 
   // implemetin repl , infinte loop until user eneter exit or closes terminl
   void repl()
   {
     string user_command;
-    while (user_command != "exit")
+    while (true)
     {
       cout << "$ ";
       getline(cin, user_command);
-      execute(user_command);
+      if(!execute(user_command)) break; // exit case
     }
+  }
+
+  // checking for file and executable permisiion
+  bool check_file_execute(string filepath)
+  {
+    if (fs::exists(filepath))
+    {
+      auto perms = fs::status(filepath).permissions();
+      if (((perms & (fs::perms::owner_exec | fs::perms::group_exec | fs::perms::others_exec)) != fs::perms::none) && fs::is_regular_file(filepath))
+      {
+        return true;
+      }
+    }
+    return false;
   }
 };
 
