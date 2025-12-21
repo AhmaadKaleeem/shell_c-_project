9aa6cf346509db189ba31dab6e82d326cbcf132e
Ahmad <Ahmadkaleeem1@gmail.com>
2025-12-21 06:06:16 +0500

    Implemented pwd

--- src/main.cpp
+++ src/main.cpp
@@ -158,6 +158,7 @@ public:
   }
 
   void execute_cd(const string& command){
+    string home;
     if (command.empty() || command == "~"){
       const char* path = getenv("HOME");
         if(!path) {path = "/";}
